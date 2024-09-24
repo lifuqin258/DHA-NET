a2ca886aab82c662c8b6680e60c47f72cf1fe32f
2031829740@qq.com <2031829740@qq.com>
2024-09-24 14:57:47 +0800

    logs

---
+++ Fusion_network.py
@@ -0,0 +1,77 @@
+import torchvision.models as models
+import torch
+import torch.nn as nn
+
+class y_glcm(nn.Module):
+    def __init__(self):
+        super(y_glcm, self).__init__()
+        self.conv = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        x = x - x.mean(dim=(2, 3), keepdim=True)
+        cov = torch.einsum('bikj,bilk->bijl', x, x)
+        cov = cov / (x.size(2) * x.size(3) - 1)
+        x = self.conv(cov)
+        return x
+
+class EMA(nn.Module):
+    def __init__(self, channels, c2=None, factor=32, num_classes=117):
+        super(EMA, self).__init__()
+        alexnet = models.alexnet(pretrained=True)
+        alexnet.eval()
+        self.feature_extractor = alexnet.features
+        self.y_glcm = y_glcm()
+        self.groups = factor
+        assert channels // self.groups > 0
+        self.softmax = nn.Softmax(-1)
+        self.dropout = nn.Dropout(p=0.6)
+        self.agp = nn.AdaptiveAvgPool2d((1, 1))
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc_0 = nn.Sequential(
+            nn.Linear(channels // self.groups, channels // self.groups, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channels // self.groups, channels // self.groups, bias=False),
+            nn.Sigmoid()
+        )
+        self.gn = nn.GroupNorm(channels // self.groups, channels // self.groups)
+
+        self.conv3x3 = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=3, stride=1, padding=1)
+        self.channel_compress = nn.Conv2d(512 // self.groups, 256 // self.groups, kernel_size=1)
+        self.fc = nn.Sequential(nn.Conv2d(channels // self.groups, channels // self.groups, 1, bias=False),
+                                nn.ReLU(),
+                                nn.Conv2d(channels // self.groups, channels // self.groups, 1, bias=False))
+        self.fc_1 = nn.Linear(channels * 6 * 6, 117)
+
+    def forward(self, x, y):
+        y_glcm = self.y_glcm(y)
+        x = self.feature_extractor(x)
+        y = self.feature_extractor(y)
+        y_glcm = self.feature_extractor(y_glcm)
+        b, c, h, w = x.size()
+        group_x = x.reshape(b * self.groups, -1, h, w)  # b*g,c//g,h,w
+        b_g, c_g, _, _ = group_x.size()
+        x = self.avg_pool(group_x).view(b_g, c_g)
+        x = self.fc_0(x).view(b_g, c_g, 1, 1)
+
+
+        group_y = y.reshape(b * self.groups, -1, h, w)
+        group_y_glcm = y_glcm.reshape(b * self.groups, -1, h, w)
+        group_y_glcm = torch.cat([self.conv3x3(group_y), group_y_glcm], dim=1)
+        group_y_glcm = self.fc(self.channel_compress(group_y_glcm))
+        avg_out = self.fc(self.avg_pool(group_y))
+        max_out = self.fc(self.max_pool(group_y))
+        y1 = self.gn(group_y * (avg_out + max_out + group_y_glcm).sigmoid())
+
+        x11 = self.softmax(self.agp(x).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x12 = y1.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
+        x21 = self.softmax(self.agp(y1).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x22 = x.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
+        weights = (torch.matmul(x11, x12) + torch.matmul(x21, x22)).reshape(b * self.groups, 1, h, w)
+        x = (group_x * weights.sigmoid() + group_y * weights.sigmoid()).reshape(b, c, h, w)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc_1(x)
+        return x
+
---
+++ train.py
@@ -0,0 +1,100 @@
+import torch.optim as optim
+from Fusion_network_8 import *
+from data_loader import *
+import matplotlib.pyplot as plt
+import time
+from sklearn.metrics import classification_report, precision_score, recall_score, f1_score
+
+train_acc_list = []
+test_acc_list = []
+
+start_time = time.time()
+
+model = EMA(channels=256)
+if torch.cuda.is_available():
+    model = model.cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.000005)
+
+# 训练模型
+for epoch in range(60):
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    print('Epoch {}/{}'.format(epoch, 60))
+    model.train()
+    for data in EMA_train_dataloader:
+        vision_inputs, touch_inputs, labels = data
+        if torch.cuda.is_available():
+            vision_inputs = vision_inputs.cuda()
+            touch_inputs = touch_inputs.cuda()
+            labels = labels.cuda()
+
+        optimizer.zero_grad()
+        outputs = model(vision_inputs, touch_inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+    train_acc = 100 * correct / total
+    print(f'Train Accuracy of the model on the train images: {train_acc} %')
+    train_acc_list.append(train_acc)
+    torch.save(model.state_dict(), 'G:\\model\\aunoglcm.pth')
+
+
+    correct = 0
+    total = 0
+    model.eval()
+    all_preds = []
+    all_labels = []
+    correctly_classified = []
+    true_labels_correct = []
+    with torch.no_grad():
+        for data in EMA_test_dataloader:
+            vision_inputs, touch_inputs, labels = data
+            if torch.cuda.is_available():
+                vision_inputs = vision_inputs.cuda()
+                touch_inputs = touch_inputs.cuda()
+                labels = labels.cuda()
+            outputs = model(vision_inputs, touch_inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            all_preds.extend(predicted.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            for i in range(len(predicted)):
+                if predicted[i] == labels[i]:
+                    correctly_classified.append(predicted[i].item())
+                    true_labels_correct.append(labels[i].item())
+
+    test_acc = 100 * correct / total
+    print(f'Test Accuracy of the model on the test images: {test_acc} %')
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total number of parameters: {total_params}")
+    test_acc_list.append(test_acc)
+    test_precision = precision_score(all_labels, all_preds, average='weighted')
+    test_recall = recall_score(all_labels, all_preds, average='weighted')
+    test_f1 = f1_score(all_labels, all_preds, average='weighted')
+
+    print(f'Test Precision: {test_precision}')
+    print(f'Test Recall: {test_recall}')
+    print(f'Test F1 Score: {test_f1}')
+print('Finished Training')
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Total training time: {:.2f} seconds'.format(elapsed_time))
+
+plt.figure(figsize=(12, 6))
+plt.plot(train_acc_list, label='Train Accuracy')
+plt.plot(test_acc_list, label='Test Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs. Epoch\nTotal training time: {:.2f} seconds'.format(elapsed_time))
+plt.legend()
+plt.grid(True)
+plt.show()
